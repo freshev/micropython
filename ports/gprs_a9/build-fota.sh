a36e955a2caa064e9632744507dd1fd6c34f2af4
server3 <user@server3>
2025-07-12 12:12:38 +0000

    OTA

--- ports/gprs_a9/build-fota.sh
+++ ports/gprs_a9/build-fota.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-#WWW_PATH=/var/opt/asque/firmware/Device_FW
-VERSION=$1
+VERSION="$1"
 VERSION="${VERSION//\"/}"
+ZIP_COMPRESS="$2"
+ZIP_COMPRESS="${ZIP_COMPRESS//\"/}"
+WWW_PATH="$3"
+WWW_PATH="${WWW_PATH//\"/}"
 
 CFG_RELEASE=debug
 if [[ "$1xx" == "releasexx" ]]; then
@@ -45,16 +48,29 @@ if [[ "${FOLDER_NAME}" != "gprs_a9" ]]; then
 fi
 
 python ${LODCOMBINE_TOOL} gen_ota --platform ${PLATFORM_LOD_FILE} --lod ${WITH_PLT_LOD_FILE} --out ${WITH_PLT_OTA_FILE}
+if [ "${PLATFORM}" = "Windows_NT" ] ; then
+    dos2unix ${WITH_PLT_OTA_FILE}
+fi
 
 for f in ${VERSION_PATH}/*.lod;
 do
     OLDVER=`echo $f | awk '{print substr($$1, length($$1) - 7, 4)}'`
     if [[ ${OLDVER} != ${VERSION} ]]; then
         echo -e "Make fota " ${OLDVER} "to" ${VERSION} " upgrade pack...\n"
-        FOTA_PACK=${FOTA_PATH}/${FIRMWARE_NAME}_${OLDVER}_to_${VERSION}.pack
+        FOTA_FILE=${FIRMWARE_NAME}_${OLDVER}_to_${VERSION}.pack
+        FOTA_PACK=${FOTA_PATH}/${FOTA_FILE}
         WITH_PLT_OTA_OLD=${FOTA_PATH}/${FIRMWARE_NAME}_fota_${OLDVER}.lod
         ${FOTACREATE_TOOL} 4194304 65536 ${WITH_PLT_OTA_OLD} ${WITH_PLT_OTA_FILE} ${FOTA_PACK}
-        #cp ${FOTA_PACK} ${WWW_PATH}
+        #A9G FOTA does not understand deflated files
+        #if [[ ${ZIP_COMPRESS} == "y" && -d "${WWW_PATH}" ]]; then
+        #    /usr/bin/zlib-flate -compress < ${FOTA_PACK} > ${WWW_PATH}/${FOTA_FILE}
+        #fi
+        #if [[ ${ZIP_COMPRESS} != "y" && -d "${WWW_PATH}" ]]; then
+        #    cp ${FOTA_PACK} ${WWW_PATH}/${FOTA_FILE}
+        #fi
+        if [[ -d "${WWW_PATH}" ]]; then
+            cp ${FOTA_PACK} ${WWW_PATH}/${FOTA_FILE}
+        fi
     fi
 done
 
